664de04388e6c4f26b4ed8ece946f4601f9b260a
Andri <andripurwansyah@gmail.com>
2021-06-22 23:28:42 +0700

    initial commit

---
+++ while.js
@@ -0,0 +1,10 @@
+var noAngkot = 1;
+var jmlAngkot = 20;
+var jmlAngkotBeroperasi = 15;
+while (noAngkot <= jmlAngkotBeroperasi) {
+    console.log('Angkot No.' + noAngkot + ' Beroperasi Dengan Baik.');
+    noAngkot++;
+}
+for (jmlAngkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
+    console.log('Angkot No.' + noAngkot + ' Tidak Beroperasi Dengan Baik.')
+}
